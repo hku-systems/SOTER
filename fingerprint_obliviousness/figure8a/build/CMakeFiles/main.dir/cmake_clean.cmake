581fa5231c47d31800fa406423e9be90deb96a30
tianxaing999 <wsntstx@163.com>
2022-05-08 22:31:19 +0800

    nits

---
+++ fingerprint_obliviousness/figure8a/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/oblifp_vgg.cpp.o"
+  "CMakeFiles/main.dir/oblifp_vgg.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ fingerprint_obliviousness/figure8b/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/fixedfp_vgg.cpp.o"
+  "CMakeFiles/main.dir/fixedfp_vgg.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
