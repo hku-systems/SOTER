5fc8c149e6962eed74690684a2ddf2fd70edae02
tianxaing999 <wsntstx@163.com>
2022-05-13 20:22:07 +0800

    nits

--- shieldmodel/models/src/mlp/mlp.cpp
+++ shieldmodel/models/src/mlp/mlp.cpp
@@ -20,14 +20,13 @@ struct mlp : public torch::nn::Module
 {
     torch::nn::Linear fc0;
     torch::nn::Linear fc1;
-    torch::nn::Linear fc2;
     torch::nn::Linear fc3;
     torch::nn::Linear fc6;
 
     mlp():
-        fc0(200704, 8192),
-        fc1(8192, 4096),
-        fc2(4096, 512),
+        fc0(6400, 4096),
+        fc1(4096, 512),
+        // fc0(50176, 512),
         fc3(512, 128),
         fc6(128, 10)
         {
@@ -38,8 +37,10 @@ struct mlp : public torch::nn::Module
     
     torch::Tensor forward(torch::Tensor x)
     {
-        x = x.view({ -1, 448 * 448 });
+        x = x.view({ -1, 80 * 80 });
         x = torch::relu(fc0->forward(x));
+        x = torch::relu(fc1->forward(x));
+        // x = torch::relu(fc2->forward(x));
         x = torch::relu(fc3->forward(x));
         x = fc6->forward(x);
         
@@ -56,7 +57,7 @@ int main()
 
     int count = 1000;
 
-    torch::Tensor input = torch::rand({1, 1, 448, 448}).to(at::kCPU);
+    torch::Tensor input = torch::rand({1, 1, 80, 80}).to(at::kCPU);
     torch::Tensor output;
 
     double start, stop, duration;
