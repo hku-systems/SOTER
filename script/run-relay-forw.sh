a53ea67ba805c2ef4207b7ce9f8aa378aa7a570a
tianxaing999 <wsntstx@163.com>
2022-05-16 18:02:32 +0800

    ssh remote read path

--- script/run-relay-forw.sh
+++ script/run-relay-forw.sh
@@ -1,5 +1,5 @@
 echo '** This is Relay server (for) script **'
 
 if [ "$1" == "vgg" ];then
-    ssh ubuntu@192.168.122.158 "cd ~/atc22-artifact/SOTER/script;bash run-tee.sh $1"
+    ssh ubuntu@192.168.122.158 "source /etc/profile;source ~/.profile;source ~/.bashrc;bash ~/atc22-artifact/SOTER/script/run-tee.sh $1"
 fi  
