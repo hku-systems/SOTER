7e1653496bfd204b156a198cdc0146e3114969d3
tianxaing999 <wsntstx@163.com>
2022-05-16 17:06:57 +0800

    nits

--- script/run-tee.sh
+++ script/run-tee.sh
@@ -1,7 +1,8 @@
 echo '** This is TEE client script **'
 
 if [ "$1" == "vgg" ];then
-    bash ~/atc22-artifact/SOTER/teertconfig/graphene-vgg-partition/runclient.sh
+    cd ~/atc22-artifact/SOTER/teertconfig/graphene-vgg-partition
+    bash runclient.sh
     echo '** VGG inference completed **'
     ssh jianyu@10.22.1.16 "cd ~/atc22-artifact/SOTER/script;bash run-relay-back.sh"
 fi 
