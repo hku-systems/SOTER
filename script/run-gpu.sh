dacee67cb6a40cd9d684779953b4444524ae18bf
tianxaing999 <wsntstx@163.com>
2022-05-18 14:16:53 +0800

    nits

--- script/run-gpu.sh
+++ script/run-gpu.sh
@@ -1,7 +1,7 @@
 echo '** This is GPU server script **'
 sed -i 's/1/0/g' signal
 # model=("vggsoter" "vggennclave" "vggag" "mlcapsule" "alexsoter" "alexag" "ressoter" "densesoter" "mlpsoter" "resag" "denseag" "mlpag" "gpubaseline" "scp")
-model=( "denseag" "scp")
+model=("transsoter")
 for ((i = 0 ; i < ${#model[@]} ; i++))
 do
     if [ "${model[$i]}" == "vggsoter" ];then
@@ -391,5 +391,34 @@ do
     fi
     if [ "${model[$i]}" == "scp" ];then
          ssh jianyu@10.22.1.16 "bash ~/atc22-artifact/SOTER/script/run-relay-forw.sh ${model[$i]}"
+    fi
+    if [ "${model[$i]}" == "transsoter" ];then
+        var=$(head -n +1 signal)
+        echo -n "[Before running] signal =  "
+        echo $var
+        pushd /home/xian/atc22-artifact/SOTER/mtr-partition/trans-partition/cpp/soter-graphene-trans
+        bash runserver.sh
+        popd
+        ssh jianyu@10.22.1.16 "bash ~/atc22-artifact/SOTER/script/run-relay-forw.sh ${model[$i]}"
+        while :
+        do
+            sleep 1s
+            var=$(head -n +1 signal)
+            if [ "$var" == "1" ]
+            then
+                echo "[After running] Signal reset to 1 by relay script"
+                proc=$(ps aux | grep gpu_server | awk 'NR==1{print $2}')
+                kill -9 $proc  
+                echo "[After running] VGG completed. Process killed. Exit"
+                sed -i 's/1/0/g' signal
+                var=$(head -n +1 signal)
+                if [ "$var" == "0" ];then
+                    echo "[After running] Signal reset to 0 by gpu script"  
+                fi
+                break
+            else 
+                echo "[After running] Signal = 0 "  
+            fi
+        done
     fi
 done
--- script/run-tee.sh
+++ script/run-tee.sh
@@ -78,3 +78,9 @@ if [ "$1" == "scp" ];then
     echo '** Send back experimental results to 185 **'
     ssh jianyu@10.22.1.16 "cd ~/atc22-artifact/SOTER/script/data;scp ./* xian@202.45.128.185:~/atc22-artifact/SOTER/script/data"
 fi
+if [ "$1" == "transsoter" ];then
+    cd ~/atc22-artifact/SOTER/teertconfig/graphene-trans-partition
+    bash runclient.sh
+    echo '** Transformer-soter inference completed **'
+    ssh jianyu@10.22.1.16 "cd ~/atc22-artifact/SOTER/script;bash run-relay-back.sh"
+fi 
