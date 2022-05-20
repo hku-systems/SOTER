8927bebea3065c70a279b9614117eb70c8793445
tianxaing999 <wsntstx@163.com>
2022-05-20 21:22:17 +0800

    sens 0all added

--- mtr-partition/sens-vgg/0-all/run.sh
+++ mtr-partition/sens-vgg/0-all/run.sh
@@ -8,5 +8,5 @@ make
 ./main 1 > ./00-1.txt
 ./main 3 > ./00-3.txt
 ./main 6 > ./00-6.txt
-scp ./*.txt xian@202.45.128.185:~/atc22-artifact/SOTER/script/sensitivity/
+scp ./00-* xian@202.45.128.185:~/atc22-artifact/SOTER/script/sensitivity/
 ssh xian@202.45.128.185 "cd ~/atc22-artifact/SOTER/script;sed -i 's/0/1/g' signal"
