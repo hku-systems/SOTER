453fef8219fcd33e9bfdcae3642ea6630ef9d5d9
tianxaing999 <wsntstx@163.com>
2022-05-20 16:32:54 +0800

    nits

--- mtr-partition/sens-vgg/04-6/normal/vgg-partition/cpp/soter-graphene-vgg/tee_client.cc
+++ mtr-partition/sens-vgg/04-6/normal/vgg-partition/cpp/soter-graphene-vgg/tee_client.cc
@@ -280,9 +280,9 @@ struct vgg19 : public torch::nn::Module
         cv12(conv_options(512, 512, 3, 1, 1)),
         cv13(conv_options(512, 512, 3, 1, 1)),
         cv14(conv_options(512, 512, 3, 1, 1)),
-        fc0(25088, 2762),
-        fc1(2762, 2762),
-        fc2(2762, 1000)
+        fc0(25088, 2800),
+        fc1(2800, 2800),
+        fc2(2800, 1000)
         {
             register_module("c", c);
 	        register_module("cv14", cv14);
