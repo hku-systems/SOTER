aa0d5e1ae6dd8a8f613c2edaf8cebc9c42081e8c
tianxaing999 <wsntstx@163.com>
2022-05-20 15:13:47 +0800

    nits

---
+++ mtr-partition/sens-vgg/02-6/high/vgg-partition/cpp/soter-graphene-vgg/cmake/build/CMakeFiles/tee_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tee_client.dir/tee_client.cc.o"
+  "CMakeFiles/tee_client.dir/tee_client.cc.o.d"
+  "tee_client"
+  "tee_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tee_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mtr-partition/sens-vgg/02-6/normal/vgg-partition/graphene-vgg-partition/runclient.sh
@@ -0,0 +1,17 @@
+pushd ../cpp/soter-graphene-vgg/
+sed -i "s/xian/ubuntu/g" ./CMakeLists.txt
+
+rm -rf cmake
+mkdir -p cmake/build
+cd cmake/build
+cmake -DCMAKE_PREFIX_PATH=$MY_INSTALL_DIR ../..
+make -j
+
+sudo rm -rf /bin/tee_client
+sudo cp ./tee_client /bin/tee_client
+
+popd
+
+make clean
+SGX=1 make
+graphene-sgx ./tee_client --target=10.22.1.26:50051 > ~/atc22-artifact/SOTER/mtr-partition/sens-vgg/02-6-1.txt
